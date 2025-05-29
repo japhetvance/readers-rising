aba8d58047ef30401e57f01ae25a2fdb0160ba19
japhetvance <japs.pamonag@intelligent-ai.solutions>
2025-05-30 01:27:46 +0800

    add Client component and page to render it

---
+++ app/books/client.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+const Client = () => {
+  return (
+    <div>client</div>
+  )
+}
+
+export default Client
---
+++ app/books/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Client from './client'
+
+const page = () => {
+  return <Client />
+}
+
+export default page
